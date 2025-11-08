c00b8040cfd591cfd67b7acb6b9b115393e80af2
Niteen-K <nkgnk6@gmail.com>
2025-11-08 11:52:38 +0530

    Initial commit

---
+++ src/main/java/com/niteen/student_management/repository/StudentRepository.java
@@ -0,0 +1,19 @@
+package com.niteen.student_management.repository;
+
+import com.niteen.student_management.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    
+    // Custom query methods
+    Optional<Student> findByEmail(String email);
+    List<Student> findByDepartment(String department);
+    List<Student> findByAgeGreaterThan(Integer age);
+    boolean existsByEmail(String email);
+}
+
